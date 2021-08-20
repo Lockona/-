d469f1c5e09a15c3102ad8a981fd6531dcd73a17
Kero <2660936365@qq.com>
2021-08-20 12:08:34 +0800

    3 commit

--- Hardware/CODE/applications/Light_Sensor/light_sensor.c
+++ Hardware/CODE/applications/Light_Sensor/light_sensor.c
@@ -9,25 +9,28 @@ void light_sensor_Init()
 	__HAL_RCC_ADC12_CLK_ENABLE();
 	__HAL_RCC_GPIOA_CLK_ENABLE();
 	GPIO_handle.Pin = GPIO_PIN_0;
-	GPIO_handle.Mode = GPIO_MODE_INPUT;
+	GPIO_handle.Mode = GPIO_MODE_ANALOG;
 	GPIO_handle.Pull = GPIO_NOPULL;
 	HAL_GPIO_Init(GPIOA,&GPIO_handle);
 	ADC_handle.Instance = ADC1;
-	ADC_handle.Init.ClockPrescaler = ADC_CLOCKPRESCALER_PCLK_DIV1;
+	ADC_handle.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV8;
 	ADC_handle.Init.Resolution = ADC_RESOLUTION_16B;
 	ADC_handle.Init.ScanConvMode = ADC_SCAN_DISABLE;
 	ADC_handle.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
 	ADC_handle.Init.LowPowerAutoWait = DISABLE;
 	ADC_handle.Init.ContinuousConvMode = ENABLE;
-	ADC_handle.Init.ExternalTrigConvEdge=ADC_EXTERNALTRIGCONVEDGE_NONE;
-	ADC_handle.Init.NbrOfConversion = 1;
+	ADC_handle.Init.DiscontinuousConvMode = DISABLE;
+	ADC_handle.Init.Overrun=ADC_OVR_DATA_PRESERVED;
+	ADC_handle.Init.LeftBitShift = ADC_LEFTBITSHIFT_NONE;
+	ADC_handle.Init.ConversionDataManagement = ADC_CONVERSIONDATA_DR;
+	ADC_handle.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
 	HAL_ADC_Init(&ADC_handle);
 	ADC_chhandle.Channel = ADC_CHANNEL_0;
 	ADC_chhandle.SamplingTime = ADC_SAMPLETIME_16CYCLES_5;
-	ADC_chhandle.SingleDiff = ADC_SINGLE_ENDED;
 	ADC_chhandle.OffsetNumber = ADC_OFFSET_NONE;
+	ADC_chhandle.Rank = 1;
 	HAL_ADC_ConfigChannel(&ADC_handle,&ADC_chhandle);
+	HAL_ADCEx_Calibration_Start(&ADC_handle,0,0);
 	HAL_ADC_Start(&ADC_handle);
-	hal_adc_
 	
 }
--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# -
+![智能门禁](.assets/%E6%99%BA%E8%83%BD%E9%97%A8%E7%A6%81.png)
+
+## mqtt服务器:使用emqx作为mqtt服务器
+
+#### 	教程：https://docs.emqx.cn/broker/v4.3/#%E5%BC%80%E5%A7%8B%E4%BD%BF%E7%94%A8
+
+## 人脸识别：使用百度的人脸识别功能，需要注册账号和实名认证，免费使用的
+
+## 会限速，识别一张照片要一秒左右，只用到人脸搜索和人脸库管理功能
+
+#### 	教程：https://ai.baidu.com/ai-doc/FACE/7k37c1twu
+
+## rt-thread教程：
+
+#### 	内核视频教程：https://www.rt-thread.org/page/video.html
+
+#### 	内核实验手册：https://www.rt-thread.org/document/site/rt-thread-version/rt-thread-standard/tutorial/experimental-manual/experimental-manual.pdf
+
+#### 	rt-thread文档中心：https://www.rt-thread.org/document/site/#/rt-thread-version/rt-thread-standard/README
+
+## LVGL教程：
+
+#### 	移植教程：https://blog.csdn.net/qq_40831286/article/details/107633216
+
+#### 	LVGL中文教程手册文档：http://lvgl.100ask.org/7.11/index.html
+
+![image-20210820110925357](.assets/image-20210820110925357.png)
+
+## Applications：各设备应用程序
+
+## cpu、Kernel：rt-thread内核
+
+## DeviceDrivers、Drivers：spi、DCMI、SD等rt-thread自带设备驱动
+
+## Filesystem、fal、EasyFlash、littlefs：flash、SD文件系统所需文件
+
+## finsh：控制台
+
+## libc：rt-thread组件
+
+### STM32_HAL：STM32H7HAL库
+
+## LVGL_Core、LVGL_Driver、lvgl_font：lvgl嵌入式GUI界面程序
+
+## Touch：触摸驱动
+
+## RC522：RC522驱动
+
+## lwIP、netdev、SAL、umqtt：网络设备协议底层和中间层
+
+## Utilities、rt_ota：文件传输组件，用于更新wifi蓝牙固件
+
+## WICED：wifi6212函数库
+
+## cJSON：json库
